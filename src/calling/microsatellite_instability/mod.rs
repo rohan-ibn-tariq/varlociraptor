bb140c7927bdbd89dd250aea996619dbfee50e44
rohan-ibn-tariq <178284954+rohan-ibn-tariq@users.noreply.github.com>
2026-05-05 17:17:10 +0500

    feat/msi-preprocess-analysis-split: refactor calling msi mod.rs

--- src/calling/microsatellite_instability/mod.rs
+++ src/calling/microsatellite_instability/mod.rs
@@ -17,30 +17,32 @@ use std::path::PathBuf;
 
 use anyhow::Result;
 use log::info;
+use rust_htslib::bcf;
 
-use crate::constants::MIN_MSI_THRESHOLD;
+use crate::cli::validate_vcf_file;
+use crate::constants::{
+    DEFAULT_SLIDING_WINDOW_SIZE,
+    MIN_MSI_THRESHOLD
+};
 use crate::errors::Error;
+use crate::utils::bcf_utils::is_phred_scaled_from_path;
 
 /* ======== CLI CONFIGURATION ===================== */
 
 /// Configuration for MSI calling pipeline.
 ///
-/// Contains all parameters needed for the MSI calling workflow,
+/// Contains all parameters needed for per-sample MSI analysis,
 /// populated from CLI arguments.
 #[derive(Debug)]
 pub(crate) struct MSIConfig {
-    /// Path to VCF/BCF file(should be sorted) with variant calls.
+    /// Path to VCF/BCF file with variant calls.
     pub calls: PathBuf,
-    /// Number of threads (None = use rayon default).
-    pub threads: Option<usize>,
+    /// Sample name to analyze from VCF/BCF.
+    pub sample: String,
+    /// Event names to combine for MSI probability (e.g., ["somatic_tumor", "high_vaf"].
+    pub events: Vec<String>,
     /// MSI-High classification threshold (percentage), default: 3.5.
     pub msi_threshold: f64,
-    /// Sample name to process from VCF/BCF.
-    pub sample: String,
-    /// Event types to consider for MSI calling (e.g. "Somatic", "Germline").
-    pub events: Vec<String>,
-    /// Whether the probabilities in the VCF are PHRED-scaled.
-    pub is_phred: bool,
     /// Allele frequency thresholds to consider for AF evolution analysis
     /// when generating pseudotime outputs. (default: [1.0,0.8,0.6,0.4,0.2,0.0])
     /// If no pseudotime outputs are requested, this will be set to [0.0] to optimize computation.
@@ -48,8 +50,11 @@ pub(crate) struct MSIConfig {
     /// as the this field is hidden constant set at CLI level. So future changes to expose this field
     /// to users should include validation for this field.
     pub af_thresholds: Vec<f64>,
-    /// Sliding window size (in base pairs) for MSI score calculation for heatmap. If None, default is used.
+    /// Sliding window size (bp) for regional MSI heatmap analysis (default: 1,000,000).
+    /// Only used if --plot-heatmap or --data-heatmap specified.
     pub sliding_window: Option<u64>,
+    /// Number of threads (None = use rayon default).
+    pub threads: Option<usize>,
     /// Output path for distribution plot (Vega-Lite JSON).
     pub plot_distribution: Option<PathBuf>,
     /// Output path for pseudotime plot (Vega-Lite JSON).
@@ -62,9 +67,31 @@ pub(crate) struct MSIConfig {
     pub data_pseudotime: Option<PathBuf>,
     /// Output path for heatmap data (TSV).
     pub data_heatmap: Option<PathBuf>,
+    /// Whether the probabilities in the VCF are PHRED-scaled.
+    pub is_phred: bool,
 }
 
 impl MSIConfig {
+    /// Set default values for MSIConfig fields based on functional logic.
+    /// Defaults Set:
+    /// 1. Set sliding window to default if not specified and heatmap output(s) requested.
+    /// 2. Set is_phred based on events sepcification.
+    pub fn set_defaults(&mut self) -> Result<()> {
+        if (self.plot_heatmap.is_some() || self.data_heatmap.is_some())
+            && self.sliding_window.is_none()
+        {
+            self.sliding_window = Some(DEFAULT_SLIDING_WINDOW_SIZE);
+        }
+
+        self.is_phred = is_phred_scaled_from_path(&self.calls)?;
+        info!(
+            "  - Probabilities are {} scaled",
+            if self.is_phred { "PHRED" } else { "linear" }
+        );
+
+        Ok(())
+    }
+
     /// Validate the MSI configuration.
     /// Responsibilities:
     /// 1. Checks for valid MSI threshold;
@@ -89,7 +116,7 @@ impl MSIConfig {
         }
 
         // Use existing validators from cli.rs to validate file format.
-        crate::cli::validate_vcf_file(&self.calls)?;
+        validate_vcf_file(&self.calls)?;
 
         Ok(())
     }
